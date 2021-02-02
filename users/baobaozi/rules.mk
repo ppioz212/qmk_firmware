c036fa2fd9ba2f24164e5bcf683586657c94fa7f
ppioz212 <adityasuresh25@gmail.com>
2021-02-01 21:56:49 -0500

    committing initial setup

---
+++ users/baobaozi/rules.mk
@@ -0,0 +1,25 @@
+SRC += baobaozi.c
+
+NKRO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+EXTRAFLAGS += -flto
+
+# CONSOLE_ENABLE = yes
+# DEBUG = yes
+
+ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
+  SRC += rgb_layers.c
+endif
+
+# ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+#     SRC += rgb_matrix.c
+# endif
+
+ifeq ($(strip $(COMBO_ENABLE)), yes)
+    SRC += combos.c
+endif
+
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+    SRC += tap_dances.c
+endif
